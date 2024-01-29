388b11db238ea3f51795da1847de7ed100da16cc
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-29 03:53:53 +0100

    Ajout d'un gif animé dans la page d'accueil

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <h3>RP Template maker</h3>
       <p>Facilite toi la vie en intégrant tes RP dans tes <Link href="/collection" >templates</Link> quali'</p>
+      <img class="image-accueil" src="https://i.pinimg.com/originals/7b/df/16/7bdf164aaed98d4336084d0b33e3e588.gif" alt="Accueil"></img>
     </main>
   );
 }
