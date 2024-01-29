cdb58019fe6c4f2dfd2277d8eaf5e7fb0eafc450
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-29 04:20:59 +0100

    Révision du texte d'introduction

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>Marre de galérer à enfiler tes <Link href="/collection" >templates</Link> ?</p>
+      <p>Marre de galérer à enfiler tes <Link href="/collection" >templates</Link> pour tes RP qui prennent du gras ?</p>
       <img class="image-accueil" src="https://media1.giphy.com/media/xT5LMsC6iydeziO6nC/200.gif?cid=82a1493baynkr03fjtz56mng389rr1aakwxo642wx8sdxqoz&ep=v1_gifs_search&rid=200.gif&ct=g" alt="Accueil"></img>
     </main>
   );
